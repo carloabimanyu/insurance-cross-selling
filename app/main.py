bc095955d0282a41f1aa02e9c2a0073ed4876b3b
devthoriq <genius.tsy.2013@gmail.com>
2024-09-06 14:45:10 +0700

    add fastapi code

---
+++ app/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import numpy as np
+
+app = FastAPI()
+
+with open("../notebooks/insurance_cross_sell_model.pkl", 'rb') as file:
+    model = pickle.load(file)
+
+class CustomerData(BaseModel):
+    Gender: int
+    Age: int
+    Driving_License: int
+    Region_Code: int
+    Previously_Insured: int
+    Vehicle_Age: int
+    Vehicle_Damage: int
+    Annual_Premium: float
+    Policy_Sales_Channel: int
+    Vintage: int
+
+@app.post("/predict/")
+async def predict(data: CustomerData):
+    input_data = [[
+        int(data.Gender), int(data.Age), int(data.Driving_License), int(data.Region_Code),
+        int(data.Previously_Insured), int(data.Vehicle_Age), int(data.Vehicle_Damage),
+        float(data.Annual_Premium), int(data.Policy_Sales_Channel), int(data.Vintage)
+    ]]
+    
+    prediction = model.predict(input_data)
+    
+    prediction_python = prediction[0].item() if isinstance(prediction[0], np.generic) else prediction[0]
+    if prediction_python==0:
+        prediction_converted="Customer is not interested"
+    else: prediction_converted="Customer is interested"
+    
+    return {"prediction": prediction_converted}
